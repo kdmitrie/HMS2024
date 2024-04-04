ce874b4273a9f73daaa34c2f74f590a66f5dc44a
kdmitrie <presentatio@mail.ru>
2024-04-04 23:05:23 +0300

    Delete %%time directive from model_5

--- solutions/model_5.py
+++ solutions/model_5.py
@@ -518,7 +518,6 @@ def create_spectogram_competition(spec_id, seconds_min):
     return img
 
 # %% [code] {"execution":{"iopub.status.busy":"2024-04-04T18:54:27.435508Z","iopub.execute_input":"2024-04-04T18:54:27.436027Z","iopub.status.idle":"2024-04-04T18:54:28.165057Z","shell.execute_reply.started":"2024-04-04T18:54:27.435991Z","shell.execute_reply":"2024-04-04T18:54:28.163258Z"}}
-%%time
 from tqdm import tqdm
 import pandas as pd
 import cv2
